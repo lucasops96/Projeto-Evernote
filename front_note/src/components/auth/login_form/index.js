b8755ab4bc718d7c46db8eaff81835bfd630ea3f
João Lucas <lucasbanana96@gmail.com>
2022-08-18 17:06:01 -0300

    create login

---
+++ front_note/src/components/auth/login_form/index.js
@@ -0,0 +1,82 @@
+import React, { Fragment, useState } from 'react';
+import { Button, Field, Control, Input, Column, Help, Label } from "rbx";
+import { Navigate } from "react-router-dom";
+import UsersService from '../../../services/users';
+
+function LoginForm() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirectToRegister, setRedirectToRegister] = useState(false);
+    const [redirectToNotes, setRedirectToNotes] = useState(false);
+    const [error, setError] = useState(false);
+
+    const HandleSubmit = async (evt)=>{
+        evt.preventDefault()
+
+        try {
+            await UsersService.login({email: email, password: password})
+            setRedirectToNotes(true)
+        } catch (error) {
+            setError(true)
+        }
+    }
+
+
+    if(redirectToRegister)
+        return <Navigate to={{pathname: "/register"}}/>
+    else if(redirectToNotes)
+        return <Navigate to={{pathname: "/notes"}}/>
+
+    return (
+        <Fragment>
+            <Column.Group centered>
+                <form onSubmit={HandleSubmit}>
+                    <Column size={12}>
+                        <Field>
+                            <Label size="small">Email:</Label>
+                            <Control>
+                                <Input
+                                    type="email"
+                                    required
+                                    name="email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </Control>
+                        </Field>
+                        <Field>
+                            <Label size="small">Password:</Label>
+                            <Control>
+                                <Input
+                                    type="password"
+                                    required
+                                    name="password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                            </Control>
+                        </Field>
+                        <Field>
+                            <Control>
+                                <Column.Group breakpoint="mobile">
+                                    <Column>
+                                        <a href='/register' className="button is-white has-text-custom-purple"
+                                            onClick={e=>setRedirectToRegister(true)}
+                                        >Register or</a>
+                                    </Column>
+                                    <Column>
+                                        <Button color="custom-purple" outlined>Login</Button>
+                                    </Column>
+                                </Column.Group>
+                            </Control>
+                        </Field>
+                        { error && <Help color="danger">Email or Password invalid</Help> }
+                    </Column>
+                </form>
+            </Column.Group>
+        </Fragment>
+    )
+}
+
+export default LoginForm;
--- front_note/src/screens/auth/login/index.js
+++ front_note/src/screens/auth/login/index.js
@@ -1,9 +1,41 @@
-import React , {Fragment } from 'react'
+import React, { Fragment } from 'react'
+import Header from '../../../components/header'
+import { Column, Section, Title, Container, Card } from 'rbx'
+import logoImage from '../../../assets/images/logo.png'
+import '../../../styles/auth.scss'
+import LoginForm from '../../../components/auth/login_form'
 
-
-const LoginScreen = ()=>(
+const LoginScreen = () => (
     <Fragment>
-        Hello login
+        <Header />
+        <Section size="medium" className="auth">
+            <Container>
+                <Column.Group centered>
+                    <Column size={3}>
+                        <Card>
+                            <Card.Content>
+                                <Section>
+                                    <Column.Group centered>
+                                        <Column size={12}>
+                                            <img src={logoImage} alt="logo" />
+                                        </Column>
+                                    </Column.Group>
+
+                                    <Column.Group>
+                                        <Column size={12}>
+                                            <Title size={6} className="has-text-grey has-text-centered">
+                                                Your notes on the cloud
+                                            </Title>
+                                        </Column>
+                                    </Column.Group>
+                                    <LoginForm/>
+                                </Section>
+                            </Card.Content>
+                        </Card>
+                    </Column>
+                </Column.Group>
+            </Container>
+        </Section>
     </Fragment>
 )
 
