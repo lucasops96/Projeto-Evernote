98a9e45259ff8de71605fbde465cb85401d5f7e3
João Lucas <lucasbanana96@gmail.com>
2022-08-20 15:13:02 -0300

    sidebar

--- front_note/src/components/header_logged/index.js
+++ front_note/src/components/header_logged/index.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from 'react'
-import { Navbar, Container, Column, Button, Dropdown } from 'rbx'
+import React, { useState } from 'react'
+import { Navbar, Column, Button, Dropdown } from 'rbx'
 import logoImage from '../../assets/images/logo-white.png'
 import "../../styles/header.scss"
 import UsersService from '../../services/users'
@@ -40,6 +40,17 @@ function HeaderLogged(props) {
             </Navbar.Brand>
 
             <Navbar.Menu>
+                <Navbar.Segment as="div" className="navbar-item navbar-start" align="start">
+                    <Navbar.Item as="div">
+                        <Button
+                            className="open-button"
+                            color="white"
+                            outlined
+                            onClick={() => props.setIsOpen(true)}>
+                            <FontAwesomeIcon icon={faList} />
+                        </Button>
+                    </Navbar.Item>
+                </Navbar.Segment>
                 <Navbar.Segment as="div" className="navbar-item navbar-end" align="right">
                     <Navbar.Item as="div">
                         <Dropdown>
