68fc248ed6f74420f0b69684b673a5349a501076
João Lucas <lucasbanana96@gmail.com>
2022-08-20 01:26:34 -0300

    header logged

--- front_note/src/screens/notes/index/index.js
+++ front_note/src/screens/notes/index/index.js
@@ -1,8 +1,9 @@
 import React , {Fragment } from 'react'
-
+import HeaderLogged from '../../../components/header_logged'
 
 const NotesScreen = ()=>(
     <Fragment>
+        <HeaderLogged/>
         Hello notes
     </Fragment>
 )
